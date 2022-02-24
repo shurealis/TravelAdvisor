731b7b008583fe8ad5a576553204b928cdbfd8a5
Bryan P <99640919+shurealis@users.noreply.github.com>
2022-02-24 22:17:19 +0700

    Add files via upload
    1:23:04

---
+++ src/api/index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+    try {
+        // request
+        const { data: { data } } = await axios.get(URL, {
+            params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng,
+            },
+            headers: {
+                'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+                'x-rapidapi-key': '75c959817amsha07074f3f6b0c7fp13d2cdjsna1136d65a67f'
+            }
+        });
+
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,20 +6,24 @@ import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('min-width: 600px');
-    const coordinates = { lat: 0, lng: 0 };
 
     return (
         <div className={classes.mapContainer}>
-            <GoogleMapReact bootstrapURLKeys={{ key: 'AIzaSyBDOgmj_PGo_V6cxm3xqTzXbFLBla-b8Oc' }}
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBDOgmj_PGo_V6cxm3xqTzXbFLBla-b8Oc' }}
                 defaultCenter={coordinates}
                 center={coordinates}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
                 options={''}
-                onChange={''}
+                onChange={(e) => {
+                    console.log(e);
+                    setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                }}
                 onChildClick={''}>
 
             </GoogleMapReact>
